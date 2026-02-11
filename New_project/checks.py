352b9b6005d250613854233b08535613675f9284
feigy2002 <feigy2131@gmail.com>
2026-02-11 16:51:37 +0200

    add a port find function

--- New_project/checks.py
+++ New_project/checks.py
@@ -1,5 +1,10 @@
 from reader import import_file
 
-def find_ip(phta):
-    a = import_file(phta)
+def find_ip(path):
+    a = import_file(path)
     return [x for x in a if not x[1].startswith(("192.168.", "10."))]
+
+def find_port(path):
+    a = import_file(path)
+    return [x for x in a if x[3] in ('22', '23', '3389')]
+
--- New_project/main.py
+++ New_project/main.py
@@ -1,9 +1,10 @@
 from reader import import_file
-from checks import find_ip
+from checks import find_ip,find_port
 
 a = 'C:/Intel/pycharm/pythonProject8/New_project/network_traffic.log'
+print(find_port(a))
 
-print(find_ip(a))
+
 
 
 
