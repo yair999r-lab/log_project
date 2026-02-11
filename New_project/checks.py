71f60a77feea82af87de353454ee61dc670f8bc8
feigy2002 <feigy2131@gmail.com>
2026-02-11 20:37:17 +0200

    add a information of zise function

--- New_project/checks.py
+++ New_project/checks.py
@@ -1,13 +1,20 @@
 from reader import import_file
 
-def find_ip(path):
+
+
+def find_bad_ip(path):
     a = import_file(path)
     return [x for x in a if not x[1].startswith(("192.168.", "10."))]
 
-def find_port(path):
+def find_bad_port(path):
     a = import_file(path)
     return [x for x in a if x[3] in ('22', '23', '3389')]
 
-def find_size(path):
+def find_big_size(path):
     a = import_file(path)
     return [x for x in a if int(x[-1]) > 5000]
+
+def list_ip_plus_size(path):
+    a = import_file(path)
+    return [x + ["LARGE"] if int(x[-1]) > 5000 else x + ["NORMAL"] for x in a]
+
--- New_project/main.py
+++ New_project/main.py
@@ -1,9 +1,12 @@
+import main
 from reader import import_file
-from checks import find_ip,find_port,find_size
+from checks import find_bad_ip,find_bad_port,find_big_size,list_ip_plus_size
 
 a = 'C:/Intel/pycharm/pythonProject8/New_project/network_traffic.log'
-print(find_size(a))
 
+
+if __name__ == '__main__':
+    print()
 
 
 
